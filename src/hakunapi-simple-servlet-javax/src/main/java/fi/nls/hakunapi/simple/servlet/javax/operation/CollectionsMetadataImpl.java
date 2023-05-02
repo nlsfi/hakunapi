20033b11a2e92b8ff78a313452179b90903f4cc0
jampukka <janne.z.heikkila@gmail.com>
2023-05-02 13:32:36 +0300

    Add alternate links to /collections

--- src/hakunapi-simple-servlet-javax/src/main/java/fi/nls/hakunapi/simple/servlet/javax/operation/CollectionsMetadataImpl.java
+++ src/hakunapi-simple-servlet-javax/src/main/java/fi/nls/hakunapi/simple/servlet/javax/operation/CollectionsMetadataImpl.java
@@ -1,7 +1,6 @@
 package fi.nls.hakunapi.simple.servlet.javax.operation;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -14,11 +13,12 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
 
 import fi.nls.hakunapi.core.FeatureType;
+import fi.nls.hakunapi.core.MetadataFormat;
 import fi.nls.hakunapi.core.FeatureServiceConfig;
 import fi.nls.hakunapi.core.schemas.CollectionInfo;
 import fi.nls.hakunapi.core.schemas.CollectionsContent;
 import fi.nls.hakunapi.core.schemas.Link;
-import fi.nls.hakunapi.core.util.U;
+import fi.nls.hakunapi.core.util.Links;
 import fi.nls.hakunapi.html.model.HTMLContext;
 
 @Path("/collections")
@@ -32,19 +32,20 @@ public class CollectionsMetadataImpl {
     public CollectionsContent handleJSON(@Context UriInfo uriInfo) {
         return handle(uriInfo, MediaType.APPLICATION_JSON);
     }
-    
+
     @GET
     @Produces(MediaType.TEXT_HTML)
     public HTMLContext<CollectionsContent> handleHTML(@Context UriInfo uriInfo) {
         return new HTMLContext<>(service, handle(uriInfo, MediaType.TEXT_HTML));
     }
-    
+
     private CollectionsContent handle(UriInfo uriInfo, String contentType) {
         Map<String, String> queryParams = OperationUtil.getQueryParams(service, uriInfo);
-        String query = U.toQuery(queryParams);
-        
-        Link self = getSelfLink(query, contentType);
-        List<Link> links = Collections.singletonList(self);
+
+        String path = service.getCurrentServerURL() + "/collections";
+        List<Link> links = new ArrayList<>();
+        links.add(Links.getSelfLink(path, queryParams, contentType));
+        links.addAll(getAlternateLinks(path, queryParams, contentType));
 
         List<CollectionInfo> collections = new ArrayList<>();
         for (FeatureType ft : service.getCollections()) {
@@ -55,11 +56,22 @@ public class CollectionsMetadataImpl {
         return new CollectionsContent(links, collections);
     }
 
-    private Link getSelfLink(String query, String contentType) {
-        String href = service.getCurrentServerURL() + "/collections" + query;
-        String rel = "self";
-        String type = contentType;
-        return new Link(href, rel, type, "This document");
+    private List<Link> getAlternateLinks(String path, Map<String, String> queryParams, String contentType) {
+        return service.getMetadataFormats().stream()
+                .filter(it -> !it.contentTypes.contains(contentType))
+                .map(it -> toAlternateLink(path, queryParams, it))
+                .toList();
+    }
+
+    private Link toAlternateLink(String path, Map<String, String> queryParams, MetadataFormat format) {
+        String mimeTypeHuman = format.id;
+        String mimeType;
+        if (format == MetadataFormat.JSON) {
+            mimeType = MediaType.APPLICATION_JSON;
+        } else { // if (format == MetadataFormat.JSON) {
+            mimeType = MediaType.TEXT_HTML;
+        }
+        return Links.getAlternateLink(path, queryParams, mimeType, mimeTypeHuman);
     }
 
 }
